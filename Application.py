3d87dc1e2b9d747095ccb24f0a1c32c78c4c109b
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-03-08 22:38:18 +0100

    Basic GUI Design and Value assignments completed.

---
+++ Application.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 23:02:26 2021
+
+@author: goharshoukat
+"""
+
+from GUI_Design import Ui_Form
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
+import Linear_Airy_Wave_Solution as laws
+
+
+class Define_Properties(qtw.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.run_button.clicked.connect(self.calculate)
+        
+    def calculate(self):
+        period = float(self.ui.period_input.text())
+        amplitude = float(self.ui.amp_input.text())
+        epsilon = float(self.ui.epsilon_input.text())
+        depth = float(self.ui.depth_input.text())
+        tidal_velocity = float(self.ui.tidal_input.text())
+        width = float(self.ui.width_input.text())
+        dx = float(self.ui.dx_input.text())
+        dz = float(self.ui.dz_input.text())
+        k = period * amplitude#float(period) * float(amplitude)
+        ans_str = '{0:0.2f}'.format(k)
+       
+        
+        
+        
+
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+    widget = Define_Properties()
+    widget.show()
+    app.exec()
+    
