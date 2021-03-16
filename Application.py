Linear Library updated to include time period. Morrison fixed for creating array of KC by adjusting the KC formula. Previously wrong formula used.

Application.py
@@ -10,7 +10,7 @@ from GUI_Design import Ui_Form
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtWidgets as qtw
 import Linear_Airy_Wave_Solution as laws
-
+import numpy as np
 
 class Define_Properties(qtw.QWidget):
     def __init__(self, *args, **kwargs):
@@ -24,6 +24,8 @@ class Define_Properties(qtw.QWidget):
         period = float(self.ui.period_input.text())
         amplitude = float(self.ui.amp_input.text())
         epsilon = float(self.ui.epsilon_input.text())
+        if epsilon > 0.3:
+             qtw.QMessageBox.information(self, 'Error', 'Wave Steepness is high, Please confirm non-linearity')
         depth = float(self.ui.depth_input.text())
         tidal_velocity = float(self.ui.tidal_input.text())
         width = float(self.ui.width_input.text())
@@ -31,7 +33,9 @@ class Define_Properties(qtw.QWidget):
         dz = float(self.ui.dz_input.text())
         k = period * amplitude#float(period) * float(amplitude)
         ans_str = '{0:0.2f}'.format(k)
-       
+        qtw.QMessageBox.information(self, 'Success', ans_str)
+        x = np.arange(0, width + dx, dx)
+        field = laws.Wave_Field(depth, period, tidal_velocity, amplitude, x, dz)
         
         
         

Linear_Airy_Wave_Solution.py
@@ -22,6 +22,7 @@ class Set_Wave_Field:
     def __init__(self,depth, time_period, tide_velocity, amplitude):
         self.depth = depth
         self.tide_velocity = tide_velocity
+        self.time_period = time_period
         self.omega = 2*math.pi/time_period
         self.amplitude = amplitude
         
@@ -135,13 +136,18 @@ class Wave_Field(Set_Wave_Field):
     #correct angle and mangnitude of the quivers. 
     def plot(self, pressure, hor_velocity, vert_velocity, time):
         plt.figure()
+    
         plt.contourf(self.X, self.Z, pressure)
-        plt.colorbar()
+        plt.colorbar(label = 'Pressure [Pa]')
+        widths = np.linspace(0, 5, self.X.size)
         plt.quiver(self.X, self.Z, \
                    self.horizontal_velocity_spatial_variable(time), \
-                       self.vertical_velocity_spatial_variable(time))
+                       self.vertical_velocity_spatial_variable(time), linewidths = widths)
+            
+    
         plt.plot(self.x, self.elevation(self.x, time))
         plt.xlabel('x [m]')
         plt.ylabel('z [m]')
+        
         plt.title('T = ' + str(time) + 's')
         plt.show()

Morrison.py
@@ -50,8 +50,9 @@ class Morisons(Linear_Airy_Wave_Solution.Set_Wave_Field):
                 * (z + self.depth)) / np.sinh(self.kfromw() * self.depth)), \
                             (np.cos(self.kfromw() * x - self.omega * time)).T)
         return self.U
-    
-    def horizontal_accelration(self, x, time, z):
+    #different function from the one in the linear airy wave solutions library because in that
+    #the functions are spatially discretised. Here they are discretised in time. 
+    def horizontal_acceleration(self, x, time, z):
         self.accel = self.amplitude * pow(self.omega,2) *\
             np.dot((np.cosh(self.kfromw() * (z + self.depth))/\
             np.sinh(self.kfromw() * self.depth)), \
@@ -62,9 +63,13 @@ class Morisons(Linear_Airy_Wave_Solution.Set_Wave_Field):
         return (self.U * self.diameter / self.nu)
     
     def kc(self):
-        return (2 * np.pi * self.amplitude / self.diameter) 
+        return (self.U * self.time_period / self.diameter) 
     
     def coefficient(self, local_Re, local_kc):
+        shape_local_Re = np.shape(local_Re)
+        shape_local_kc = np.shape(local_kc)
+        local_Re = local_Re.flatten('F')
+        local_kc = local_kc.flatten('F')
         Re = np.array((-10, 1e1, 1e3, 5e5, 1e10))
         kc = np.array((-0.05, 5, 15, 50, 100))
         KC, RE = np.meshgrid(kc, Re)
@@ -85,6 +90,8 @@ class Morisons(Linear_Airy_Wave_Solution.Set_Wave_Field):
         
         ip_Cd = interpolate.interp2d(KC, RE, Cd)
         local_Cd = ip_Cd(local_Re, local_kc)
+        local_Re = local_Re.reshape(shape_local_Re[0], shape_local_Re[1])
+        local_kc = local_kc.reshape(shape_local_kc[0], shape_local_kc[1])
         return local_Cm, local_Cd
     
     def force(self, local_Cd, local_Cm):
@@ -95,6 +102,16 @@ class Morisons(Linear_Airy_Wave_Solution.Set_Wave_Field):
         self.f_total = self.f_drag + self.f_inertial
         
     
-        
+field = Morisons(50, 10, 0, 0.3, 10)
+t = field.time(20,0.5)
+x = 0
+elevation = field.elevation(0,t)
+field.diffraction_effects()
+z_array = field.z_array(50, 0.5)
+hor_velocity = field.horizontal_velocity(x, t, z_array)
+a = field.horizontal_acceleration(x, t, z_array)
+Re = field.Reynolds()
+kc = field.kc()
+coefficient = field.coefficient(Re, kc)
 
 